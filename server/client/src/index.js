c191b7d07eb17205025c1682023971a976dc4e91
David <davidgbanks91@gmail.com>
2018-01-30 17:29:01 -0800

    React project with routes

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './components/App';
+import reducers from './reducers';
+const store = createStore(reducers, {}, applyMiddleware());
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
+);
--- server/services/passport.js
+++ server/services/passport.js
@@ -20,17 +20,17 @@ passport.use(
     callbackURL: '/auth/google/callback',
     proxy: true
   }, 
-  (accessToken, refreshToken, profile, done) => { 
-    User.findOne({ googleId: profile.id }).then(existingUser => {
-      if (existingUser) {
-        done(null, existingUser);
-      } else {
-        new User({ 
-          googleId: profile.id,
-          firstName: profile.name.givenName
-        }).save().then(newUser => done(null, newUser));
-      }
-    });
+  async (accessToken, refreshToken, profile, done) => { 
+    const existingUser = await User.findOne({ googleId: profile.id });
+    if (existingUser) {
+      done(null, existingUser);
+    } else {
+      const newUser = await new User({ 
+        googleId: profile.id,
+        firstName: profile.name.givenName
+      }).save();
+      done(null, newUser);
+    }
   })
 );
 
