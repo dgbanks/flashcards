6747fbff1f8edfc40cbec31fffd54b79b4cc745f
David <davidgbanks91@gmail.com>
2018-01-31 22:29:47 -0800

    Header with materialize CSS

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './header';
 
 const Landing = () => <h2>Landing</h2>
-const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route exact path="/" component={Landing} />
           <Route path="/dashboard" component={Dashboard} />
         </div>
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import 'materialize-css/dist/css/materialize.min.css'; // if not js, you DO have to include the extension
+
 import App from './components/App';
 import reducers from './reducers';
+
 const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
